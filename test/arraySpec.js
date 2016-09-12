127d825dc3939e2b072a15cbbfa2144964c04e87
Vitaly Tomilov <vitaly.tomilov@gmail.com>
2016-09-12 20:19:35 +0100

    tests update.

--- test/arraySpec.js
+++ test/arraySpec.js
@@ -1,11 +1,40 @@
 'use strict';
 
-var array = require('../lib/array');
+var parse = require('../lib/array');
 
 describe('Array/Positive', function () {
 
 });
 
 describe('Array/Negative', function () {
+
+    it("must throw on invalid input types", function () {
+        var typeErr = 'Invalid tuple array type: ';
+        expect(function () {
+            parse();
+        }).toThrow(new TypeError(typeErr + typeof undefined));
+        expect(function () {
+            parse(null);
+        }).toThrow(new TypeError(typeErr + typeof null));
+        expect(function () {
+            parse(123);
+        }).toThrow(new TypeError(typeErr + typeof 123));
+    });
+
+    it("must throw on invalid input syntax", function () {
+        var syntaxErr = 'Invalid tuple array syntax: ';
+        expect(function () {
+            parse('');
+        }).toThrow(new TypeError(syntaxErr));
+        expect(function () {
+            parse('hello');
+        }).toThrow(new TypeError(syntaxErr + 'hello'));
+        expect(function () {
+            parse('{');
+        }).toThrow(new TypeError(syntaxErr + '{'));
+        expect(function () {
+            parse('}');
+        }).toThrow(new TypeError(syntaxErr + '}'));
+    });
 
 });
