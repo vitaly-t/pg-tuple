ce69d6bd8f1d52f1bf8bd5d050634be50ba67ed1
Vitaly Tomilov <vitaly.tomilov@gmail.com>
2016-09-13 22:00:06 +0100

    adding data + preliminary tests.

--- test/arraySpec.js
+++ test/arraySpec.js
@@ -3,7 +3,34 @@
 var parse = require('../lib/array');
 
 describe('Array/Positive', function () {
-
+/*
+    it("simple arrays of values", function () {
+        expect(parse('{"(2,two)","(3,three)"}')).toEqual([
+            '(2,two)', '(3,three)'
+        ]);
+    });
+    it("composite value + array", function () {
+        expect(parse('("(7,inner)","{""(8,eight)"",""(9,nine)""}")')).toEqual([
+            '(7,inner)', '{""(8,eight)"",""(9,nine)""}'
+        ]);
+    });
+    it("fully composite arrays", function () {
+        // Q: Should we replace inner '\\"' at this point?
+        // A: Yeah, most likely!
+        expect(parse('{"(\\"(7,inner)\\",\\"{\\"\\"(88,eight-1)\\"\\",\\"\\"(99,nine-2)\\"\\"}\\")","(\\"(77,inner)\\",\\"{\\"\\"(888,eight-3)\\"\\",\\"\\"(999,nine-4)\\"\\"}\\")"}'))
+            .toEqual([
+                '(\\"(7,inner)\\",\\"{\\"\\"(88,eight-1)\\"\\",\\"\\"(99,nine-2)\\"\\"}\\")',
+                '(\\"(77,inner)\\",\\"{\\"\\"(888,eight-3)\\"\\",\\"\\"(999,nine-4)\\"\\"}\\")'
+            ]);
+    });
+    it("nested arrays", function () {
+        expect(parse('{{"(11,q1)","(22,q2)"},{"(33,e1)","(44,e2)"}}'))
+            .toEqual([
+                '{"(11,q1)","(22,q2)"}',
+                '{"(33,e1)","(44,e2)"}'
+            ]);
+    });
+*/
 });
 
 describe('Array/Negative', function () {
